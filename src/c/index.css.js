0cbd58d37739b04c74957fdf0ea8bce80c944915
hkryucr <hkryucr@gmail.com>
2019-11-18 19:07:29 -0800

    first commit

---
+++ src/c/index.css.js
@@ -0,0 +1,8 @@
+import colors from "c/core/ui-colors.css.js";
+
+export default {
+  fontFamily: "Rubik, sans-serif",
+  fontSize: "16px",
+  color: colors.text.primary,
+  backgroundColor: colors.background.primary,
+};
